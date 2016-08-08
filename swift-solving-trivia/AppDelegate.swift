a2b1f845456234b1c680ededdde2c0df33b8f0ff
bhtran <bhtran00@gmail.com>
2016-08-08 16:26:32 -0400

    Completed lab with refactoring for smaller big-O

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -12,11 +12,79 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    /*
-     
-     write your function here
-     
-     */
+    func solveTrivia(dictionary:[String:String]) -> String {
+        
+        var returnString : String = ""
+        
+//        for (state, capital) in dictionary
+//        {
+//            
+//            var matchCount : Int = 0
+//            print("\nCapital of \(state) is \(capital), it has \(capital.characters.count) letter in its name\n")
+//            
+//            var capitalCount : Int = capital.characters.count
+//
+//
+//            for stateLetter in state.lowercaseString.characters
+//            {
+//                print("\(capitalCount) is the capitalCount\n")
+//                
+//                capitalCount -= 1
+//                
+//                for capitalLetter in capital.lowercaseString.characters
+//                {
+//                   
+//                    print("Current state letter is: \(stateLetter), current capital letter is:\(capitalLetter)\n")
+//                    
+//                    if stateLetter == capitalLetter
+//                    {
+//                        matchCount += 1
+//                        print("\(stateLetter) is equal to \(capitalLetter)")
+//                        print("So far, match count is \(matchCount)")
+//                        print("It has a match, \(state) is not the answer")
+//                        
+//                    }
+//                    else if matchCount == 0 && capitalCount < 1
+//                    {
+//                        
+//                        returnString = state
+//                        
+//                        print("matchCount is:\(matchCount) and capitalCount is:\(capitalCount)")
+//                        print("\(state) wins!")
+//                    }
+//                    
+//                }
+//                
+//            }
+//            
+//        }
+//    
+//        return returnString
+   
+        for (state, capital) in dictionary {
+            
+            var matchCount : Int = 0
+            
+            var capitalCount : Int = capital.characters.count
+            
+            for stateLetter in state.lowercaseString.characters {
+                
+                capitalCount -= 1
+                    
+                    if capital.containsString(String(stateLetter)) {
+                        matchCount += 1
+                        
+                    }
+                    else if matchCount == 0 && capitalCount < 1 {
+                        returnString = state
+                    }
+                
+            }
+            
+        }
+        
+        return returnString
+    }
 
 }
 
