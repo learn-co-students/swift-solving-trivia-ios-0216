2bf09d622a667d3b981fc791b20a0150eaf1fdc8
ArielSD <arielsd@gmail.com>
2017-09-25 19:57:10 -0400

    Done

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -12,18 +12,23 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    
-    
     func solveTrivia(_ trivia: [String : String]) -> String {
-        
-        // Implement this function!
-        
-        return "Hi :D"
-        
+        for (state, capital) in trivia {
+            let lowerCaseStateCharacters = Array(state.lowercased().characters)
+            let lowerCaseCapitalCharacters = Array(capital.lowercased().characters)
+            var noMatches = true
+            
+            for character in lowerCaseCapitalCharacters {
+                if lowerCaseStateCharacters.contains(character) {
+                    noMatches = false
+                    break
+                }
+            }
+            
+            if noMatches == true {
+                return state
+            }
+        }
+        return "There is no such state"
     }
-    
-    
-    
-    
 }
-
